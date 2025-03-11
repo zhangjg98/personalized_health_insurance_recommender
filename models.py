2a01b80c225655f87214ffc30c8730e0b2b14d6c
zhangjg98 <zhangjg@csu.fullerton.edu>
2025-03-10 21:47:01 -0700

    Adjustments made to model to improve results

--- models.py
+++ models.py
@@ -14,14 +14,21 @@ class HybridRBM_SVD(nn.Module):
         
         # First hidden layer
         self.layer1 = nn.Linear(num_visible, num_hidden_1)
-        self.relu1 = nn.ReLU()
+        self.leaky_relu1 = nn.LeakyReLU(0.1)  # More stable activation than ReLU
+        self.dropout1 = nn.Dropout(0.3)  # Dropout for regularization
         
         # Second hidden layer
         self.layer2 = nn.Linear(num_hidden_1, num_hidden_2)
-        self.relu2 = nn.ReLU()
+        self.leaky_relu2 = nn.LeakyReLU(0.1)
+        self.dropout2 = nn.Dropout(0.3)
         
         # Latent layer (collaborative filtering-like)
         self.latent_layer = nn.Linear(num_hidden_2, num_latent)
+        self.leaky_relu_latent = nn.LeakyReLU(0.1)
+        
+        # Extra hidden layer after the latent representation
+        self.extra_layer = nn.Linear(num_latent, num_latent)
+        self.leaky_relu_extra = nn.LeakyReLU(0.1)
         
         # Output layer: reconstruct back to SVD space (num_visible)
         self.output_layer = nn.Linear(num_latent, num_visible)
@@ -32,12 +39,20 @@ class HybridRBM_SVD(nn.Module):
         
     def forward(self, v):
         h1 = torch.sigmoid(self.layer1(v))
-        h1 = self.relu1(h1)
-        
+        h1 = self.leaky_relu1(h1)
+        h1 = self.dropout1(h1)
+
         h2 = torch.sigmoid(self.layer2(h1))
-        h2 = self.relu2(h2)
-        
+        h2 = self.leaky_relu2(h2)
+        h2 = self.dropout2(h2)
+
         latent = self.latent_layer(h2)
+        latent = self.leaky_relu_latent(latent)
+
+        # Extra layer
+        latent = self.extra_layer(latent)
+        latent = self.leaky_relu_extra(latent)
+
         v_prob = torch.sigmoid(self.output_layer(latent))
         return v_prob
 
@@ -51,6 +66,12 @@ class HybridRBM_SVD(nn.Module):
             h2_sample = torch.bernoulli(h2)
             
             latent = self.latent_layer(h2_sample)
+            latent = self.leaky_relu_latent(latent)
+
+            # Extra transformation step
+            latent = self.extra_layer(latent)
+            latent = self.leaky_relu_extra(latent)
+
             v_prob = torch.sigmoid(self.output_layer(latent))
             v = torch.bernoulli(v_prob)
         return v0, v
