ca7faf4d30d57f6e3ec712786589e966b8c5d855
zhangjg98 <zhangjg@csu.fullerton.edu>
2025-05-05 21:38:10 -0700

    Removing unneeded file from repository

--- supabase_client.py
+++
@@ -1,7 +0,0 @@
-import os
-from supabase import create_client
-
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
-
-supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
